ed1a9e79a2087147718ed4fc2b16388c63b593e2
Peyton Vecchi <74024201+veechi@users.noreply.github.com>
2024-01-16 15:10:46 -0500

    Update and rename my-first-program.py to greet-name-age.py
    Add comments

--- greet-name-age.py
+++ greet-name-age.py
@@ -1,17 +1,20 @@
+#greet user
 print('Hello, world!')
 
+#prompt user for name
 print('What is your name?')
-
 myName = input()
 
+#greet user using name
 print('It is good to meet you, ' + myName)
 
+#inform user on the length of their name
 print('The length of your name is: ')
-
 print(len(myName))
 
+#prompt user for age
 print('\nWhat is your age?')
-
 myAge = input()
 
+#inform user on how old they will be in 1 year
 print('You will be ' + str(int(myAge) + 1) + ' in a year.')
